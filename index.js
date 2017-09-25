a30ebc01ea7b86fc3575c8f1e7862532ca55b9ad
Alvaro Uribe <alvaro.uribe@vocus.co.nz>
2017-09-25 16:02:15 +1300

    initial web scraper

---
+++ index.js
@@ -0,0 +1,72 @@
+const { get } = require('https');
+const similar = require('similarity');
+const mail = require('sendmail')();
+const { writeFile } = require('fs');
+
+const URL = 'https://www.immigration.govt.nz/new-zealand-visas/apply-for-a-visa/about-visa/argentina-whs';
+const TIMEOUT = 4000;
+const DIFFERENCE = 1;
+const FROM = 'iam@alvarouribe.cl';
+const TO = ['iam@alvarouribe.cl','uribe.gaston41@gmail.com','javieraciao@hotmail.com'];
+
+let base = '';  
+
+function run() {
+
+  get(URL, (res) => {
+    let html = ``;
+    let today = new Date();
+    console.log(`Requesting... ${today}`);
+  
+    if (res.statusCode !== 200) {
+      console.error(new Error(`Something went wrong, status ${res.statusCode}`));
+      res.resume();
+    }
+  
+    res.setEncoding('utf8');
+  
+    res.on('data', (chunk) => {
+      html += chunk;
+    });
+  
+    res.on('end', () => {
+      const difference = similar(base, html);
+
+      if (difference < DIFFERENCE) {
+        base = html;
+
+        const fileName = `${Date.now()}.html`;
+
+        writeFile(`./logs/${fileName}`, html, (err) => {
+          if (err) console.error('Error writing file:', err);
+        });
+
+        mail({
+          from: FROM,
+          to: TO,
+          subject: 'Scraper = Change Found',
+          html: 'Attached file...',
+          attachments: [
+            {   // utf-8 string as an attachment
+              filename: fileName,
+              content: html
+            }
+          ]
+        }, (err, reply) => {
+          console.error(err && err.stack);
+        });
+      }
+
+      setTimeout(() => {
+        run();
+      }, TIMEOUT);
+    });
+  
+  }).on('error', (err) => {
+    console.error(err);
+  });
+}
+
+run();
+
+console.log('Started.');
