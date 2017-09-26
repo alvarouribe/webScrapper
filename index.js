35db1d9b6f6cee3864592f26dc08d17de2d8811f
Alvaro Uribe <alvaro.uribe@vocus.co.nz>
2017-09-27 11:46:31 +1300

    do not send the first html

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ const DIFFERENCE = 1;
 const FROM = 'iam@alvarouribe.cl';
 const TO = ['iam@alvarouribe.cl','uribe.gaston41@gmail.com','javieraciao@hotmail.com'];
 
+let firstTimer = true;
 let base = '';  
 
 function run() {
-
   get(URL, (res) => {
     let html = ``;
     let today = new Date();
     console.log(`Requesting... ${today}`);
-  
+    console.log(`firstTimer: ${firstTimer}`);
+          
     if (res.statusCode !== 200) {
       console.error(new Error(`Something went wrong, status ${res.statusCode}`));
       res.resume();
@@ -30,31 +31,36 @@ function run() {
     });
   
     res.on('end', () => {
+      
       const difference = similar(base, html);
+      console.log(`difference: ${difference}`);
 
       if (difference < DIFFERENCE) {
         base = html;
+        
+        if (!firstTimer) {
+          const fileName = `${Date.now()}.html`;
 
-        const fileName = `${Date.now()}.html`;
-
-        writeFile(`./logs/${fileName}`, html, (err) => {
-          if (err) console.error('Error writing file:', err);
-        });
-
-        mail({
-          from: FROM,
-          to: TO,
-          subject: 'Scraper = Change Found',
-          html: 'Attached file...',
-          attachments: [
-            {   // utf-8 string as an attachment
-              filename: fileName,
-              content: html
-            }
-          ]
-        }, (err, reply) => {
-          console.error(err && err.stack);
-        });
+          writeFile(`./logs/${fileName}`, html, (err) => {
+            if (err) console.error('Error writing file:', err);
+          });
+        
+          mail({
+            from: FROM,
+            to: TO,
+            subject: 'Scraper = Change Found',
+            html: 'Attached file...',
+            attachments: [
+              {   // utf-8 string as an attachment
+                filename: fileName,
+                content: html
+              }
+            ]
+          }, (err, reply) => {
+            console.error(err && err.stack);
+          });
+        }
+        firstTimer = false;
       }
 
       setTimeout(() => {
