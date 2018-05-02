a3c2130bfdd1161e25d047789cac03d73f89a2c7
Alvaro Uribe <alvaro.uribe@vocus.co.nz>
2018-05-02 12:44:56 +1200

    changes made to check_for_differences

--- check_for_diferences.js
+++ check_for_diferences.js
@@ -3,11 +3,11 @@ const similar = require('similarity');
 const mail = require('sendmail')();
 const { writeFile } = require('fs');
 
-const URL = 'https://www.immigration.govt.nz/new-zealand-visas/apply-for-a-visa/about-visa/argentina-whs';
+const URL = 'https://www.nzherald.co.nz/';
 const TIMEOUT = 40000;
 const DIFFERENCE = 1;
 const FROM = 'iam@alvarouribe.cl';
-const TO = ['iam@alvarouribe.cl','uribe.gaston41@gmail.com','javieraciao@hotmail.com'];
+const TO = ['iam@alvarouribe.cl'];
 
 let firstTimer = true;
 let base = '';  
@@ -76,3 +76,9 @@ function run() {
 run();
 
 console.log('Started.');
+
+
+/*
+  This will send me an email 
+  if the URL changes. It will check for the url every 40 seconds
+*/
